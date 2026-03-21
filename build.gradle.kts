12825fe29d185746726dfbf5d53d198e6dc59802
CIORBA Antonio <antoniociorba@outlook.com>
2026-03-21 21:07:30 +0100

    chore: added gradle build and settings files

---
+++ build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    java
+    id("org.openjfx.javafxplugin") version "0.1.0" apply false
+}
+
+allprojects {
+    group = "tech.nabor"
+    version = "1.0-SNAPSHOT"
+
+    repositories {
+        mavenCentral()
+    }
+}
+
+subprojects {
+    apply(plugin = "java")
+
+    java {
+        toolchain {
+            languageVersion.set(JavaLanguageVersion.of(21))
+        }
+    }
+
+    dependencies {
+        testImplementation(platform("org.junit:junit-bom:5.10.0"))
+        testImplementation("org.junit.jupiter:junit-jupiter")
+        testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    }
+
+    tasks.test {
+        useJUnitPlatform()
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,9 @@
+rootProject.name = "Nabor_Services_Java_Client"
+
+// main app parts
+include("core-api")
+// include("core-impl")
+// include("app-desktop")
+
+// plugins
+// include("plugins:sync")
